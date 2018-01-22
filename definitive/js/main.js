f22cfca4782d1d19fe97baf52a89a43a9bc6fa97
kelvintromp <34335060+kelvintromp@users.noreply.github.com>
2018-01-22 17:01:13 +0100

    rm

--- definitive/js/main.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {							//Create a function, toggles between adding and removing responsive class to the navbar
-	var x = document.getElementById("mytopnav") //Get the ID from the html div
-	if (x.className === "topnav-menu") {		//Checks to see if get the responive class
-		x.className += " responsive ";
-	} else{
-		x.className = "topnav-menu";
-	}
-
-}
