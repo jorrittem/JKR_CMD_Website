b1dc6c531c65e6587d783a5e22913f3723e88eb7
kelvintromp <k.p.i.tromp@st.hanze.nl>
2018-01-23 15:52:10 +0100

    error fix

--- definitive/js/main.js
+++ definitive/js/main.js
@@ -1,9 +1,10 @@
 // navbar
-function myFunction() { //Create a function, toggles between adding and removing responsive class to the navbar
-	let x = document.getElementById("mytopnav") //Get the ID from the html div
-	if (x.className === "topnav-menu") { //Checks to see if get the responive class
+function myFunction() { // create a function, toggles between adding and removing responsive class to the navbar
+	let x = document.getElementById("mytopnav"); // get the ID from the html div
+	if (x.className === "topnav-menu") { // checks to see if get the responive class
 		x.className += " responsive";
-	} else{
+	}
+	else {
 		x.className = "topnav-menu";
 	}
 }
