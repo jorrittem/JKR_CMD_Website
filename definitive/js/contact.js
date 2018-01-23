0f0ca0c0244bbc80fcfaf712979ce18d3e874c95
jorrittem <j.tempels7@gmail.com>
2018-01-23 21:48:33 +0100

    Feature style and comments added

--- definitive/js/contact.js
+++ definitive/js/contact.js
@@ -1,6 +1,8 @@
 // contact feature
+//Shows the first tab when page is loaded
 document.getElementById("id").style.display = "block";
 
+//function to toggle between tabs
 function openMajor(evt, majorName) {
     var i, tabcontent, tablinks;
 
@@ -14,6 +16,7 @@ function openMajor(evt, majorName) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
+    //Shows the tab wich is active
     document.getElementById(majorName).style.display = "block";
     evt.currentTarget.className += " active";
 }
